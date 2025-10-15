ce2a3fb49172095c05f1804386ffa7ebf6b8b3c9
Daniel <danielmustafin076@gmail.com>
2025-10-15 11:00:11 +0600

    testing

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,40 @@
+// import "./globals.css";
+// import styles from './layout.module.scss'
+// export const metadata = {
+//   title: "Genery LAB",
+//   description: "Made by Genery",
+// };
+
+// export default function RootLayout({ children }) {
+//   return (
+//     <html lang="ru">
+//       <body>
+//         <div className={styles.wrapper}>
+//           <div className={styles.content}>
+//               {children}
+//           </div>
+//         </div>
+//       </body>
+//     </html>
+//   );
+// }
+// layout.js
 import "./globals.css";
-import styles from './layout.module.scss'
+import styles from './layout.module.scss';
+import { DM_Sans, Noto_Sans_Mono } from "next/font/google";
+
+const dmSans = DM_Sans({
+  subsets: ["latin", "cyrillic"],
+  weight: ["400", "500", "700"],
+  variable: "--font-dm-sans"
+});
+
+const notoMono = Noto_Sans_Mono({
+  subsets: ["latin", "cyrillic"],
+  weight: ["400", "700"],
+  variable: "--font-noto-sans-mono"
+});
+
 export const metadata = {
   title: "Genery LAB",
   description: "Made by Genery",
@@ -7,12 +42,10 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="ru">
+    <html lang="ru" className={`${dmSans.variable} ${notoMono.variable}`}>
       <body>
         <div className={styles.wrapper}>
-          <div className={styles.content}>
-              {children}
-          </div>
+          <div className={styles.content}>{children}</div>
         </div>
       </body>
     </html>
