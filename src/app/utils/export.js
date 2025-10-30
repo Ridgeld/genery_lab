a99ce8bbd22e180074d2eba576fbf36bdc2642ea
Daniel <danielmustafin076@gmail.com>
2025-10-30 08:03:15 +0600

    corrections 2

--- src/app/utils/export.js
+++ src/app/utils/export.js
@@ -1,14 +1,14 @@
-import { solveMechanism } from "./kinematics";
-import { buildVelocityForAngle } from "./kinematics";
+// import { solveMechanism } from "./kinematics";
+// import { buildVelocityForAngle } from "./kinematics";
 
 
 
-function ExportToCSV ({L1, L2, L3, L0, angle, omega}){
+// function ExportToCSV ({L1, L2, L3, L0, angle, omega}){
 
 
     
 
-    return(
+//     return(
 
-    )
-}
+//     )
+// }
